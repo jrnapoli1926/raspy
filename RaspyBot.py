204937b222d7e1ded7193392312baa29fc51f0b7
jrnapoli1926 <roccojuniorcolacicco@gmail.com>
2016-10-15 11:35:20 +0200

    Update and rename simple-bot.py to RaspyBot.py

--- RaspyBot.py
+++ RaspyBot.py
@@ -2,15 +2,17 @@ import time
 import random
 import datetime
 import telepot
+from RaspyCam.py import RaspyCam
 
 """
 After **inserting token** in the source code, run it:
-```
-$ python simple-bot.py
-```
+'''
+$ python RaspyCam.py
+'''
 This simple bot does nothing but accepts two commands:
-- `/roll` - reply with a random integer between 1 and 6, like rolling a dice.
-- `/time` - reply with the current time, like a clock.
+- '/roll' - reply with a random integer between 1 and 6, like rolling a dice.
+- '/time' - reply with the current time, like a clock.
+- '/snap' - reply with a photo from PiCamera
 """
 
 def handle(msg):
@@ -23,6 +25,10 @@ def handle(msg):
         bot.sendMessage(chat_id, random.randint(1,6))
     elif command == '/time':
         bot.sendMessage(chat_id, str(datetime.datetime.now()))
+    elif command == 'snap':
+        raspyCam = RaspyCam()
+        filename = raspyCam.capture()
+        bot.sendPhoto(chat_id, open(filename, 'rb'))
 
 bot = telepot.Bot('*** INSERT TOKEN ***')
 bot.message_loop(handle)
